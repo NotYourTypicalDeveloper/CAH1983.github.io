c7d22adb2658145f95fc35ace4e909167779c0db
CAH1983 <carolineho1983@yahoo.fr>
2018-12-19 15:22:07 +0000

    transfer new portfolio

---
+++ javascript/app.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  const slideShow =  document.querySelectorAll('.slide');
+  const next = document.querySelector('.next');
+  const prev = document.querySelector('.prev');
+  let counter = 0;
+
+
+  slideShow[0].style.display='inline-flex';
+
+
+  function moveNext() {
+    if (counter === slideShow.length-1) {
+      counter = 0;
+    } else counter++;
+
+    for(let i=0; i<slideShow.length;i++) {
+      if (i === counter) {
+        slideShow[i].style.display='flex';
+      } else  slideShow[i].style.display='none';
+    }
+  }
+  next.addEventListener('click', moveNext);
+
+  function movePrev() {
+    if (counter === 0) {
+      counter = slideShow.length-1;
+    } else counter--;
+
+    for(let i=0; i<slideShow.length;i++) {
+      if (i === counter) {
+        slideShow[i].style.display='inline-flex';
+      } else  slideShow[i].style.display='none';
+    }
+  }
+  prev.addEventListener('click', movePrev);
+
+});
